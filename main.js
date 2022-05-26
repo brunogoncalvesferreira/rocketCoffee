35c4414fada6c0c3282dad31bfd9bf7e3a53a2e1
Bruno Gonçalves Ferreira <74840490+brunogoncalvesferreira@users.noreply.github.com>
2022-05-25 21:38:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// criando a função para abrir o menu mobile
+
+function abrirMenuMobile() {
+  document.body.classList.add('active')
+}
+function fecharMenuMobile() {
+  document.body.classList.remove('active')
+}
